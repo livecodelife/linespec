fd53237bda9724dc4b51dcd12243c6e801218f28
Caleb Cowen <calebcowen@gmail.com>
2026-04-03 11:57:10 -0500

    Add provenance record for lock-layer feature [prov-2026-7df318ca]

---
+++ pkg/provenance/lock_layer_test.go
@@ -0,0 +1,13 @@
+package provenance
+
+import (
+	"testing"
+)
+
+func TestLockLayer(t *testing.T) {
+	// Tests for lock-layer command will be added here
+}
+
+func TestCheckLockedScope(t *testing.T) {
+	// Tests for locked scope lint rule will be added here
+}
